e426756d36a17a6e251b46400c382deb0702c3db
WhalerX <xwk1497726741@163.com>
2026-05-22 16:16:23 +0800

    新一次结构测试

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CODE/C++/Proj/ScoreManagement/src/main.cpp" "CMakeFiles/app.dir/src/main.cpp.obj" "gcc" "CMakeFiles/app.dir/src/main.cpp.obj.d"
+  "D:/CODE/C++/Proj/ScoreManagement/src/manager.cpp" "CMakeFiles/app.dir/src/manager.cpp.obj" "gcc" "CMakeFiles/app.dir/src/manager.cpp.obj.d"
+  "D:/CODE/C++/Proj/ScoreManagement/src/student.cpp" "CMakeFiles/app.dir/src/student.cpp.obj" "gcc" "CMakeFiles/app.dir/src/student.cpp.obj.d"
+  "D:/CODE/C++/Proj/ScoreManagement/src/ui.cpp" "CMakeFiles/app.dir/src/ui.cpp.obj" "gcc" "CMakeFiles/app.dir/src/ui.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
