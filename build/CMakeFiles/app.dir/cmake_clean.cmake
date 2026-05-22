c1def4b7e9f3aa102d34ce6bdebb65618473309f
WhalerX <xwk1497726741@163.com>
2026-05-22 15:42:30 +0800

    结构的排列(完整版)

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/main.cpp.obj"
+  "CMakeFiles/app.dir/src/main.cpp.obj.d"
+  "CMakeFiles/app.dir/src/manager.cpp.obj"
+  "CMakeFiles/app.dir/src/manager.cpp.obj.d"
+  "CMakeFiles/app.dir/src/student.cpp.obj"
+  "CMakeFiles/app.dir/src/student.cpp.obj.d"
+  "CMakeFiles/app.dir/src/ui.cpp.obj"
+  "CMakeFiles/app.dir/src/ui.cpp.obj.d"
+  "D:/CODE/C++/Proj/ScoreManagement/bin/app.exe"
+  "D:/CODE/C++/Proj/ScoreManagement/bin/app.exe.manifest"
+  "D:/CODE/C++/Proj/ScoreManagement/bin/app.pdb"
+  "D:/CODE/C++/Proj/ScoreManagement/bin/libapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
